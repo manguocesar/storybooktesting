6564cd7907b7bdfde89396472ad05aec762646a6
Cesar <cesar.hertz@icloud.com>
2025-01-23 13:24:30 +0100

    Update Storybook configuration, add Task component and stories, and enhance styles

---
+++ src/components/Task.stories.jsx
@@ -0,0 +1,59 @@
+import { fn } from "@storybook/test";
+
+
+import Task from './Task';
+
+export const ActionsData = {
+    onArchiveTask: fn(),
+    onPinTask: fn(),
+};
+
+export default {
+    component: Task,
+    title: 'Task',
+    tags: ['autodocs'],
+    //👇 Our exports that end in "Data" are not stories.
+    excludeStories: /.*Data$/,
+    args: {
+        ...ActionsData,
+    },
+};
+
+export const Default = {
+    args: {
+        task: {
+            id: '1',
+            title: 'Test Task',
+            state: 'TASK_INBOX',
+        },
+    },
+};
+
+export const Pinned = {
+    args: {
+        task: {
+            ...Default.args.task,
+            title: 'Test Task (pinned)',
+            state: 'TASK_PINNED',
+        },
+    },
+};
+
+export const Archived = {
+    args: {
+        task: {
+            ...Default.args.task,
+            state: 'TASK_ARCHIVED',
+        },
+    },
+};
+
+export const Tested = {
+    args: {
+        task: {
+            ...Default.args.task,
+            title: 'Test Task (tested)',
+            state: 'TASK_TESTED',
+        },
+    },
+};
